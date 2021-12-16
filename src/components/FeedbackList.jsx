ea39f24af873e158274989a58b92a87136ed16fc
dale <stewartdale45@yahoo.com>
2021-12-16 11:34:05 -0500

    Created my first component and props

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import FeedbackItem from './FeedbackItem';
+
+function FeebackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+  //   console.log(feedback);
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem 
+        key={item.id} 
+        item={item} 
+        handleDelete={handleDelete}/>
+      ))}
+    </div>
+  );
+}
+
+FeedbackItem.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FeebackList;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+        {
+            id: 1,
+            rating: 10,
+            text: 'Explore the fascinating world of Lesbianism',
+        },
+        {
+            id: 2,
+            rating: 8,
+            text: 'Ass licking lesbian milfs',
+        },
+        {
+            id: 3,
+            rating: 10,
+            text: 'Tounge teasing ebony massive breasted lesbian lovers'
+        }
+    ]
+
+    export default FeedbackData;
