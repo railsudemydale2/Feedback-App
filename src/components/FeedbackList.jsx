46d31b1e98b1ed7acdfad6eeeb03d75b96542882
dale <stewartdale45@yahoo.com>
2021-12-18 23:22:19 -0500

    Installed npm package called framer-motion and used it to show Fade Animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
@@ -6,16 +7,37 @@ function FeebackList({ feedback, handleDelete }) {
     return <p>No Feedback Yet</p>;
   }
   //   console.log(feedback);
+
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem 
-        key={item.id} 
-        item={item} 
-        handleDelete={handleDelete}/>
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       <FeedbackItem
+  //       key={item.id}
+  //       item={item}
+  //       handleDelete={handleDelete}/>
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackItem.propTypes = {
@@ -29,11 +51,3 @@ FeedbackItem.propTypes = {
 };
 
 export default FeebackList;
-
-
-
-
-
-
-
-
